661a36037d026f19f4c61e8b9f16bdec45e90ed0
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-06-19 01:57:36 -0300

    Estrutura condicional - Exercicío 06

--- 02 - Estrutura condicional/src/application/Exercicio04.java
+++ 02 - Estrutura condicional/src/application/Exercicio04.java
@@ -4,30 +4,35 @@ import java.util.Scanner;
 
 public class Exercicio04 {
 
+	/*
+	 * Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do
+	 * jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo
+	 * uma duração mínima de 1 hora e máxima de 24 horas.
+	 */
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int horaInicial;
 		int horaFinal;
-		
+
 		System.out.println("Digite a hora inicial do jogo: ");
 		horaInicial = sc.nextInt();
-		
+
 		System.out.println("Digite a hora final do jogo: ");
 		horaFinal = sc.nextInt();
-		
+
 		int duracao;
-		
+
 		if (horaInicial < horaFinal) {
-			duracao = horaFinal - horaInicial;   
+			duracao = horaFinal - horaInicial;
 		} else {
-			  duracao = 24 - horaInicial + horaFinal;
+			duracao = 24 - horaInicial + horaFinal;
 		}
-		
+
 		System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
-		
 
 	}
 
--- 02 - Estrutura condicional/src/application/Exercicio05.java
+++ 02 - Estrutura condicional/src/application/Exercicio05.java
@@ -4,47 +4,52 @@ import java.util.Scanner;
 
 public class Exercicio05 {
 
+	/*
+	 * Com base na tabela abaixo, escreva um programa que leia o código de um item e
+	 * a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+	 */
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int codItem, quantItem;
-		
+
 		double valorTotal = 0;
-		
+
 		System.out.println("Digite o código do item: ");
 		codItem = sc.nextInt();
-		
+
 		System.out.println("Digite a quantidade de item: ");
 		quantItem = sc.nextInt();
-		
+
 		if (codItem == 1) {
 			valorTotal = quantItem * 4;
 			System.out.println("Quantidade de cachorro quente: " + quantItem);
 			System.out.println("valor a pagar: " + valorTotal);
-			
+
 		} else if (codItem == 2) {
 			valorTotal = quantItem * 4.50;
 			System.out.println("Quantidade de X-Salada: " + quantItem);
 			System.out.println("Valor a pagar: " + valorTotal);
-			
+
 		} else if (codItem == 3) {
 			valorTotal = quantItem * 5.00;
 			System.out.println("Quantidade de X-Bacon: " + quantItem);
 			System.out.println("Valor a pagar: " + valorTotal);
-			
+
 		} else if (codItem == 4) {
 			valorTotal = quantItem * 2.00;
 			System.out.println("Quantidade de Torrada simples: " + quantItem);
 			System.out.println("Valor a pagar: " + valorTotal);
-			
+
 		} else if (codItem == 5) {
 			valorTotal = quantItem * 1.50;
 			System.out.println("Quantidade de Refrigerante: " + quantItem);
 			System.out.println("Valor a pagar: " + valorTotal);
 		}
-		
+
 		System.out.println("-----------------------------------------------");
 		System.out.println("Valor total " + valorTotal);
 
